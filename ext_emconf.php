96f9a353a45103651280b0b89526d92a8e3144ac
Markus Hofmann <m.hofmann@web-vision.de>
2025-03-07 10:34:51 +0100

    [TASK] Add emconf for TER releases

---
+++ ext_emconf.php
@@ -0,0 +1,36 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'DeepL Write',
+    'description' => 'DeepL Write support for TYPO3. Write better texts, translate to simple language',
+    'category' => 'backend',
+    'author' => 'web-vision GmbH Team',
+    'author_company' => 'web-vision GmbH',
+    'author_email' => 'hello@web-vision.de',
+    'state' => 'stable',
+    'version' => '5.0.1',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.1.0-8.4.99',
+            'typo3' => '12.4.0-13.4.99',
+            'backend' => '12.4.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [
+            'container' => '*',
+            'dashboard' => '*',
+            'install' => '*',
+            'enable_translated_content' => '*',
+        ],
+    ],
+    // @todo Autoload section in `ext_emconf.php` should not be needed anymore since TYPO3 v12, and in first test it
+    //       showed to work without it. This should be verified debugging TYPO3 core to be really sure about that and
+    //       making additional tests. Scheduled to be removed if removal can be considered safe.
+    'autoload' => [
+        'psr-4' => [
+            'WebVision\\DeeplWrite\\' => 'Classes',
+            'DeepL\\' => 'vendor/deeplcom/deepl-php/src',
+            'Http\\Discovery\\' => 'vendor/php-http/discovery/src',
+        ],
+    ],
+];

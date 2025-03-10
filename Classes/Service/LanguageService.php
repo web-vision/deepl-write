03557a8894404bb13c9e86377820e8a69a6c91fe
Markus Hofmann <m.hofmann@web-vision.de>
2025-03-10 14:24:26 +0100

    [TASK] Add DeepL Write Service wrapper
    This wrapper service deals with the API and is preparation for later
    points of checks.

---
+++ Classes/Service/LanguageService.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebVision\DeeplWrite\Service;
+
+use TYPO3\CMS\Core\Site\Entity\SiteInterface;
+use WebVision\DeeplWrite\Domain\Enum\RephraseSupportedDeepLLanguage;
+
+final class LanguageService
+{
+    public function getTargetLanguageForRephrasing(SiteInterface $site, int $targetLanguage): ?RephraseSupportedDeepLLanguage
+    {
+        $targetLanguage = $site->getLanguageById($targetLanguage);
+        $targetLanguageConfiguration = $targetLanguage->toArray();
+        $deeplTargetLanguage = $targetLanguageConfiguration['deeplTargetLanguage'];
+
+        return RephraseSupportedDeepLLanguage::tryFrom($deeplTargetLanguage);
+    }
+}

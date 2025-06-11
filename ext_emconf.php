cb853f978a9e65f57fdcb4784a240fcde94beafc
Stefan Bürk <stefan@buerk.tech>
2025-06-11 18:08:55 +0200

    [TASK] Set required `deepl-base` version
    This change ensures that at least 1.0.1 of
    the `deepl-base` extension is installed as
    lowest version.
    
    Used command(s):
    
    ```shell
    BIN_COMPOSER="$(which composer)" && \
    ${BIN_COMPOSER} require 'web-vision/deepl-base':'^1.0.1'
    ```

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
             'php' => '8.1.0-8.4.99',
             'typo3' => '12.4.0-13.4.99',
             'backend' => '12.4.0-13.4.99',
-            'deepl_base' => '*',
+            'deepl_base' => '1.0.1-1.99.99',
             'deeplcom_deepl_php' => '1.12.0-1.12.99',
         ],
         'conflicts' => [],
